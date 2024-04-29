20aad41913e02b4da43430b715aa376d668a3183
mihajasoa <mihajasoaalain85@gmail.com>
2024-04-29 19:35:21 +0200

    fix:layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Footer, Navbar } from "@/components";
-
+import LayoutRoot from "@/components/layout/LayoutRoot";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,9 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
-        {children}
-        <Footer/>
+        <LayoutRoot>{children}</LayoutRoot>
         </body>
     </html>
   );
