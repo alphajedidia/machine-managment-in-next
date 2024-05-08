2e5699eeade9a4ee6d9ee2b8765489ebd2cfff28
mihajasoa <mihajasoaalain85@gmail.com>
2024-05-08 20:56:38 +0300

    add:layoutAdmin

--- src/components/layout/LayoutAdmin.tsx
+++ src/components/layout/LayoutAdmin.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import Navbar from "../navbar/Navbar";
-import Footer from "../Footer";
-import ProtectedRoute from "../ProtectedRoute";
 import { SessionProvider } from "next-auth/react";
 const LayoutAdmin = ({ children }: { children: React.ReactNode }) => {
   return (
 <SessionProvider>
-
     {children}
-
 </SessionProvider>
   );
 };
