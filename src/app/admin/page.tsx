cef01ad1dde49aa588e331f5f4c9761e0d72d6d1
mihajasoa <mihajasoaalain85@gmail.com>
2024-05-10 11:40:46 +0300

    add:admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,10 @@
-'use client'
+"use client";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import CardInfo from "@/components/cardInfo/CardInfo";
 import BarChart from "@/components/charts/BarChart";
 import { signOut } from "next-auth/react";
 import { Engin } from "@prisma/client";
+import ChartThree from "@/components/charts/Chart.Donut";
 export default function ProtectedPage() {
   const revenueData = {
     labels: [
@@ -25,20 +26,20 @@ export default function ProtectedPage() {
       92000, 78000,
     ],
   };
-  const mockData: Engin [] = [
+  const mockData: Engin[] = [
     {
       matricule: "ENG001",
       id_type: "TYPE001",
       id_entrepot: "ENT001",
       etat: true,
-      
     },
     {
       matricule: "ENG002",
       id_type: "TYPE002",
       id_entrepot: "ENT002",
       etat: false,
-    },{
+    },
+    {
       matricule: "ENG002",
       id_type: "TYPE002",
       id_entrepot: "ENT002",
@@ -47,19 +48,21 @@ export default function ProtectedPage() {
   ];
   return (
     <ProtectedRoute>
+      <div >
         <h1>Page protégée</h1>
-        <p>Cette page est accessible uniquement aux utilisateurs authentifiés.</p>
-
-
+        <p>
+          Cette page est accessible uniquement aux utilisateurs authentifiés.
+        </p>
         <div className="flex">
-      <div className="w-[700px]">
-        <h2>Tableau de bord</h2>
-        <BarChart data={revenueData} />
+          <div className="w-[700px]">
+            <h2>Tableau de bord</h2>
+            <BarChart data={revenueData} />
+          </div>
+          <CardInfo titre={"Top Engin"} detail={mockData} />
+        </div>
+        <ChartThree />
+        <button onClick={() => signOut()}>Sign OUT</button>
       </div>
-      <CardInfo titre={"Top Engin"} detail={mockData} />
-    </div>
-
-        <button onClick={()=>signOut()}>Sign OUT</button>
     </ProtectedRoute>
   );
 }
