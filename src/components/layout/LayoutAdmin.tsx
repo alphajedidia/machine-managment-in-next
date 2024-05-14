b8f2bf21177ec06c03ca56b70b1f8127b03e0768
mihajasoa <mihajasoaalain85@gmail.com>
2024-05-14 23:02:03 +0300

    fix:layoutAdmin

--- src/components/layout/LayoutAdmin.tsx
+++ src/components/layout/LayoutAdmin.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { SessionProvider } from "next-auth/react";
+import ProtectedRoute from "../ProtectedRoute";
+import NavBarAdmin from "../NavBar.admin/NavBar.admin";
+import SideBar from "../SideBar/SideBar";
 const LayoutAdmin = ({ children }: { children: React.ReactNode }) => {
+  console.log('admin')
   return (
-<SessionProvider>
-    {children}
-</SessionProvider>
+    <SessionProvider>
+      <ProtectedRoute>
+        <NavBarAdmin NavTitle={"GESTION DE LOCATION D'ENGIN"} />
+        <SideBar />
+        <div className="pt-20 pl-72">
+        {children}
+        </div>
+      </ProtectedRoute>
+    </SessionProvider>
   );
 };
 
