c9ed58d187bc42ea998d91010368dba369d5334b
mihajasoa <mihajasoaalain85@gmail.com>
2024-05-17 08:06:39 +0300

    fix:cardBody

--- src/components/cardInfo/CardBody.tsx
+++ src/components/cardInfo/CardBody.tsx
@@ -8,7 +8,7 @@ interface CardBodyProps {
 const CardBody: React.FC<CardBodyProps> = ({ data }) => {
   return (
 
-      <ul className='  p-3 text-xl w-auto max-h-44  border overflow-auto'>
+      <ul className='  p-3 text-xl w-auto h-full border overflow-auto'>
         {data.map((item) => (
           <li className='p-2' key={item.matricule}>
             <p>{item.matricule} {item.id_type} {item.etat} <span className='underline hover:text-red-900 cursor-pointer'>detail</span></p>
