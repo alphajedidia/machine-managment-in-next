b7637346b9e4b293753fd7edbdb112dd410eec53
mihajasoa <mihajasoaalain85@gmail.com>
2024-06-03 12:36:01 +0300

    fix table

--- src/app/admin/location/page.tsx
+++ src/app/admin/location/page.tsx
@@ -1,14 +1,20 @@
-import React from "react"; 
-export default function Engin() {
+import { locationData, locationInfo } from "@/app/utils/data";
+import CardInfo from "@/components/card.info/CardInfo";
+import Location from "@/components/location/Location";
+import TableLocation from "@/components/location/TableLocation";
+import React from "react";
+const page = () => {
   return (
-    <div className="flex items-center">
-                        <div className="h-2.5 w-2.5 rounded-full bg-red-500 me-2"></div> Offline
-
-    <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Online
-</div>
-
-
-
-
+    <div className="flex flex-col flex-grow justify-around px-5 h-full w-full">
+      <div className=" flex justify-between pt-5 ">
+        {locationInfo.map((value) => {
+          return <CardInfo data={value.total} dataLabel={value.label} />;
+        })}
+      </div>
+      <div className="w-full border shadow-lg rounded-lg">
+        <TableLocation Title={"Liste des locations d'engin"} />
+      </div>
+    </div>
   );
-}
+};
+export default page;
