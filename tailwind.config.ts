5853a380f3a45301b5f79bae4bf20ec6a0d162c2
alphajedidia <alphajedidia01@gmail.com>
2024-06-05 16:09:14 +0300

    add: datepicker in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,15 @@
 import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
+
+
 
 const config: Config = {
+  darkMode: 'selector',
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
   ],
   theme: {
     extend: {
