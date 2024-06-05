e7945e53584693d8eba91e482c30f12a93c66c1f
alphajedidia <alphajedidia01@gmail.com>
2024-06-05 16:12:13 +0300

    styled : card

--- src/components/Engin/Card.tsx
+++ src/components/Engin/Card.tsx
@@ -9,7 +9,7 @@ const Card = ({ title, description1, prixJournalier }: CardData) => {
     <div className=" max-w-80  flex h-fit flex-wrap  rounded-xl overflow-hidden shadow-lg mt-6 mx-3 my-4">
       <div>
         <img src="G.jpeg" alt="img" />
-        <div className=" mx-6 my-2 h-[120px] ">
+        <div className=" mx-6 my-3 h-[120px] ">
           <div>
             <h3 className=" font-black text-secondary-700 text-xl">{title}</h3>
             <p className=" font-semibold mt-2">
@@ -24,7 +24,7 @@ const Card = ({ title, description1, prixJournalier }: CardData) => {
         </div>
         <div className="flex justify-between mt-5 items-center mr-6 mb-5 mx-6 ">
             <CustomButton title="Reserver" containerStyles=" bg-primary-500 px-4 py-2 rounded-lg text-lg font-bold hover:scale-105 transition-all" iconAfter={<Cart iconStyle="w-4"/>}/>
-            <a href={title} className=" underline">Voir l'engin</a>
+            <a href={title} className=" underline hover:scale-105 ">Voir l'engin</a>
           </div>
       </div>
     </div>
