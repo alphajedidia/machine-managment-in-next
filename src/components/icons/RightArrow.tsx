8961184da6cb4a2ca5b22efb3caa477304e98abe
alphajedidia <alphajedidia01@gmail.com>
2024-06-06 03:54:37 +0300

    Add Right Arrow Icon

---
+++ src/components/icons/RightArrow.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const RightArrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+      />
+    </svg>
+  );
+};
+
+export default RightArrow;
