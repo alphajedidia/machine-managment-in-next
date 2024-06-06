421cc19189cd08d2f0f4317389eeb24bc5d5b773
alphajedidia <alphajedidia01@gmail.com>
2024-06-06 08:10:33 +0300

    edit : the Engin page into modal

--- src/app/Engin/page.tsx
+++ src/app/Engin/page.tsx
@@ -16,49 +16,44 @@ const Engin = ({
   prix_journalier,
 }: IEngin) => {
   return (
-    <div className=" w-2/3  mx-auto py-40">
-      <div className="flex bg-white  rounded-xl overflow-hidden shadow-lg">
+      <div className="flex bg-white  rounded-xl overflow-hidden">
       <div className="w-3/6 ">
         {/* <img src={image_url} alt={nom_engin} /> */}
         <img src="G.jpeg" alt="" className=" h-full " />
       </div>
       <div className=" w-3/6    ">
-        <CustomButton
-          title="X"
-          containerStyles=" float-right px-6 font-bold py-4 bg-gray-100"
-        />
         <h3 className=" font-extrabold text-3xl mx-12 my-4 text-secondary-600">
-          Test
+          {nom_engin}
         </h3>
         <div className=" mx-12">
           <h3 className="text-2xl font-semibold">Spécifications</h3>
           <div className="flex  border-b pb-2 pt-2 text-lg border-secondary-600 mt-2">
             <p className="w-1/3 font-semibold ">Catégorie :</p>
-            <p className="w-2/3">Poids Lourd</p>
+            <p className="w-2/3">{description}</p>
           </div>
           <div className="flex  border-b pb-2 pt-2 text-lg border-secondary-600 mt-2">
             <p className="w-1/3 font-semibold ">Poids :</p>
-            <p className="w-2/3">50 250 kg</p>
+            <p className="w-2/3">{description}</p>
           </div>
           <div className="flex  border-b pb-2 pt-2 text-lg border-secondary-600 mt-2">
             <p className="w-1/3 font-semibold ">Moteur :</p>
-            <p className="w-2/3">Diesel</p>
+            <p className="w-2/3">{description}</p>
           </div>
           <div className="flex  border-b pb-2 pt-2 text-lg border-secondary-600 mt-2">
             <p className="w-1/3 font-semibold ">Consommation :</p>
-            <p className="w-2/3">10 Litres/heure</p>
+            <p className="w-2/3">{description}</p>
           </div>
           <div className="flex  border-b pb-2 pt-2 text-lg border-secondary-600 mt-2">
             <p className="w-1/3 font-semibold ">Nombre de place :</p>
-            <p className="w-2/3">2</p>
+            <p className="w-2/3">{description}</p>
           </div>
           <div className="flex  border-b pb-2 pt-2 text-lg border-secondary-600 mt-2">
             <p className="w-1/3 font-semibold ">Vitesse Max:</p>
-            <p className="w-2/3">6 km/h</p>
+            <p className="w-2/3">{description}</p>
           </div>
           <div className="flex  border-b pb-2 pt-2 text-lg border-secondary-600 mt-2">
             <p className="w-1/3 font-semibold  ">Prix:</p>
-            <p className="w-2/3">1 000 000 Ariary/Jour</p>
+            <p className="w-2/3">{prix_journalier}</p>
           </div>
           <div className="flex items-center justify-between">
           
@@ -72,7 +67,6 @@ const Engin = ({
         </div>
       </div>
       </div>
-    </div>
   );
 };
 
