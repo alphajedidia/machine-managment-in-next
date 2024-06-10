6958279833dce419f3e6d882de7654246347b17b
alphajedidia <alphajedidia01@gmail.com>
2024-06-10 21:31:44 +0300

    add : date in the localstorage

--- src/app/client/layout.tsx
+++ src/app/client/layout.tsx
@@ -2,18 +2,27 @@
 
 import { Footer, Navbar } from "@/components";
 import { CardData } from "@/components/Engin/data";
+import { showSuccesToCart } from "@/utils/sweetAlertUtils";
 import { createContext, useEffect, useState } from "react";
+import { DateType } from "react-tailwindcss-datepicker";
+import Swal from "sweetalert2";
 
 export const CartContext = createContext<{
+  date: { startDate: null; endDate: null };
+  setDate: React.Dispatch<React.SetStateAction<{ startDate: null; endDate: null }>>;
   cartCount: number;
   setCartCount: React.Dispatch<React.SetStateAction<number>>;
   cartItems: CardData[];
   setCartItems: React.Dispatch<React.SetStateAction<CardData[]>>;
+  removeFromCart: (item: CardData) => void;
 }>({
+  date: { startDate: null, endDate: null },
+  setDate: () => {},
   cartCount: 0,
   setCartCount: () => {},
   cartItems: [],
   setCartItems: () => {},
+  removeFromCart: () => {},
 });
 
 export default function RootLayout({
@@ -21,6 +30,11 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [date, setDate] = useState<{ startDate: null; endDate: null }>(() => {
+    const savedDate = localStorage.getItem("date");
+    return savedDate ? JSON.parse(savedDate) : { startDate: null, endDate: null };
+  });
+  
   const [cartCount, setCartCount] = useState<number>(() => {
     const savedCartCount = localStorage.getItem("cartCount");
     return savedCartCount ? Number(savedCartCount) : 0;
@@ -32,18 +46,52 @@ export default function RootLayout({
   });
 
   useEffect(() => {
+    localStorage.setItem("date", JSON.stringify(date));
     localStorage.setItem("cartCount", String(cartCount));
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
-  }, [cartCount, cartItems]);
+  }, [date, cartCount, cartItems]);
 
+  const removeFromCart = (item: CardData) => {
+    const updatedCartItems = cartItems.filter((cartItem) => cartItem !== item);
+    Swal.fire({
+      title: "Supprimer l'engin de la liste",
+      text: "",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#e7473c",
+      cancelButtonColor: "#c8c8c8",
+      confirmButtonText: "Supprimer",
+      cancelButtonText: "Annuler",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Supprimer de la liste avec succès",
+          text: "",
+          icon: "success",
+          timer : 1000,
+          showConfirmButton: false,
+        });
+        setCartItems(updatedCartItems);
+        setCartCount(updatedCartItems.length);
+      }
+    });
+  };
 
   return (
     <html lang="en">
       <body className="">
-      <CartContext.Provider
-          value={{ cartCount, setCartCount, cartItems, setCartItems }}
+        <CartContext.Provider
+          value={{
+            date,
+            setDate,
+            cartCount,
+            setCartCount,
+            cartItems,
+            setCartItems,
+            removeFromCart,
+          }}
         >
-          <Navbar cartCount={cartCount} cartItems={cartItems} />
+          <Navbar cartCount={cartCount} />
           {children}
           <Footer />
         </CartContext.Provider>
