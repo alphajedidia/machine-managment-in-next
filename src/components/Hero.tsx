d34f866dbfef81a9853ae9ced72464849f5c87ae
alphajedidia <alphajedidia01@gmail.com>
2024-06-11 13:41:57 +0300

    Styled : the hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,21 +10,21 @@ function Hero() {
   
   }
   const heroValue = {
-    header : "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-    paragraph : "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autemnecessitatibus explicabo ipsa alias aliquam ipsam dolores! ",
+    header : "Engins de Qualité",
+    paragraph : "Nous sommes une entreprise spécialisée dans la location d'engins. Notre objectif est de fournir les meilleurs services à nos clients.",
     cta : "Voir nos engins"
   }
   return (
     <>
-      <div className=" flex h-[80vh] space-x-16 pb-16 mt-16 pt-12">
+      <div className=" bg-white flex h-[80vh] space-x-16 pb-16 mt-16 pt-12">
         <div className=" w-1/2 pl-32 pt-24 pr-6">
-          <h1 className=" text-6xl">
+          <h1 className=" text-7xl font-extrabold leading-tight">
             {heroValue.header}
           </h1>
-          <p className=" text-3xl mt-6 ">
+          <p className=" text-4xl mt-12 font-medium">
             {heroValue.paragraph}
           </p>
-          <CustomButton title={heroValue.cta} iconAfter={<Arrow iconStyle="w-6" />} containerStyles= " bg-primary-500 text-secondary-600 px-4 py-2 rounded-md mt-16 mb-5 text-2xl shadow-md hover:scale-105 hover:shadow-lg transition-all " handleClick={handleScroll}/>
+          <CustomButton title={heroValue.cta} iconAfter={<Arrow iconStyle="w-6" />} containerStyles= " font-bold bg-primary-500 text-secondary-600 px-4 py-2 rounded-md mt-16 mb-5 text-2xl shadow-md hover:scale-105 hover:shadow-lg transition-all " handleClick={handleScroll}/>
         </div>
         <div className=" w-2/5  bg-slate-400 border border-red-700 mr-28 mt-12 mb-4">
           {/* <Model/> */}
