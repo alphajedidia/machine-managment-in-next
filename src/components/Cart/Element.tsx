1823b1b6fc402e5bfb0bb19dc973b9b28ac53ff8
alphajedidia <alphajedidia01@gmail.com>
2024-06-11 11:31:37 +0300

    styled : the element

--- src/components/Cart/Element.tsx
+++ src/components/Cart/Element.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import { CardData } from '@/components/Engin/data'
-import CustomButton from '../CustomButton'
-import { Delete } from '../icons'
 
-const Element = ({title, description1, prixJournalier} : CardData ) => {
+
+const Element = ({title,imgUrl,category, description1, prixJournalier} : CardData ) => {
   return (
         <div className=' flex'>
-        <img src="G.jpeg" alt="test" className='w-48 h-28' />
-        <div className=' font-black text-lg my-auto mx-6'>
+         <img src={`../${imgUrl}`} alt="img" className="w-48 h-28" />
+        <div className=' font-black text-md my-auto mx-6'>
             <p className=' text-xl'>{title}</p>
-            <p> Moteur : <span className=' font-normal'>{description1}</span></p>
+            <p> Moteur : <span className=' font-normal'>{description1.split("*")[0]}</span></p>
             <p> Prix Journalier : <span className=' font-normal'>{prixJournalier}</span></p>
         </div>
         </div>
